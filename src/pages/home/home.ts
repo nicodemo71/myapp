93e3b6a9687dd749f715ad082b377081e04a0e8e
Nicola Lo Piccolo <nlopiccolo@gmail.com>
2017-04-27 18:15:18 +0200

    nfc testing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,14 +8,17 @@ import {Platform} from "ionic-angular/index";
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, platform: Platform) {
+
+  constructor(public navCtrl: NavController, platform: Platform, private nfc: NFC) {
     platform.ready().then(()=>{
-      console.log('ready to go');
+      //console.log('ready to go');
+      this.addNfcListeners();
+      nfc.addTagDiscoveredListener((tagEvent:Event) => this.tagListenerSuccess(tagEvent));
     })
   }
 
   addNfcListeners(): void {
-    NFC.addTagDiscoveredListener((tagEvent:Event) => this.tagListenerSuccess(tagEvent));
+    //NFC.addTagDiscoveredListener((tagEvent:Event) => this.tagListenerSuccess(tagEvent));
   }
 
   tagListenerSuccess(tagEvent:Event) {
